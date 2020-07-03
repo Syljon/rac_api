9f592a08e8c54e9cde3ab60f1ad7b7d1b838eaed
Lukasz Siwiec <lukasz.siwiec@euvic.pl>
2020-07-03 18:29:36 +0200

    add connect to database

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
 import express, { Application } from "express";
 import testRoutes from "./routes/test";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const port: number = Number(process.env.PORT) || 5000;
 const app: Application = express();
 
+dotenv.config();
+
+mongoose
+  .connect(
+    process.env.DB_CONNECT as string,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    async () => {
+      console.log("connected to db!");
+    }
+  )
+  .finally(async () =>
+    app.listen(port, () => console.log("Server Up and running ", port))
+  );
+
+app.use(express.json());
+
 app.use("/test", testRoutes);
-app.listen(port, () => console.log("Server Up and running ", port));

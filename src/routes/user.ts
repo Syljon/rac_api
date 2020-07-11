6dbf985e277c0e1ea0090d765365530701f34106
Lukasz Siwiec <lukasz.siwiec@euvic.pl>
2020-07-11 20:04:31 +0200

    create auth routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,13 +1,27 @@
 import express, { Router } from "express";
 import * as UserController from "../controller/user";
+import validate from "../helpers/validate";
+import * as yup from "yup";
 
 const router: Router = express.Router();
 
+const getUser = yup.object({
+  email: yup.string().required(),
+  password: yup.string().required(),
+});
+
+const postUser = yup.object({
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  email: yup.string().required(),
+  role: yup.string().required(),
+});
+
 router.get("/", UserController.getUsers);
 
-router.get("/:id", UserController.getUser);
+router.get("/:id", validate(getUser), UserController.getUser);
 
-router.post("/", UserController.postUser);
+router.post("/", validate(postUser, "body"), UserController.postUser);
 
 router.delete("/:id", UserController.deleteUser);
 

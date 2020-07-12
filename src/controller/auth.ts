7eb24e8fa835f9ca0a669ed8893bd7387d0aaf30
Lukasz Siwiec <lukasz.siwiec@euvic.pl>
2020-07-12 13:16:35 +0200

    set-password endpoint

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -1,10 +1,16 @@
 import User, { IUser } from "../model/user";
 import bcrypt from "bcryptjs";
-import { createToken } from "../helpers/token";
+import { createToken, decodeToken } from "../helpers/token";
 
 type RequestBody = {
   email: string;
   password: string;
+};
+
+type SETPAsswordBody = {
+  token: string;
+  password: string;
+  password2: string;
 };
 
 export async function login(req: any, res: any) {
@@ -19,4 +25,27 @@ export async function login(req: any, res: any) {
   }
   const token = createToken(user);
   return res.header("auth-token", token).send(token);
+}
+
+export async function setPassword(req: any, res: any) {
+  const body = req.body as SETPAsswordBody;
+  const { email } = decodeToken(body.token);
+
+  const existingUser = await User.findOne({ email: email });
+
+  if (!existingUser)
+    return res.status(400).send("User with that email dosne't exists");
+
+  existingUser.hashPassword = await bcrypt.hash(
+    body.password,
+    await bcrypt.genSalt(10)
+  );
+  existingUser.emailConfirmed = true;
+
+  try {
+    const savedUser = await existingUser.save();
+    res.send(savedUser);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 }
